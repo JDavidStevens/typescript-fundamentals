6ac8c460a60ffde22d110baa2051b6b7b86f70ec
David Stevens <jdavid.stevens@gmail.com>
2019-05-29 09:41:00 -0600

    config mods & advanced types

--- notes/7-advanced-types.ts
+++ notes/7-advanced-types.ts
@@ -4,33 +4,33 @@ import { HasEmail, HasPhoneNumber } from "./1-basics";
  * (1) MAPPED TYPES allow the use of an interface to transform keys into values
  */
 
-// interface CommunicationMethods {
-//   email: HasEmail;
-//   phone: HasPhoneNumber;
-//   fax: { fax: number };
-// }
+interface CommunicationMethods {
+  email: HasEmail;
+  phone: HasPhoneNumber;
+  fax: { fax: number };
+}
 
-// function contact<K extends keyof CommunicationMethods>(
-//   method: K,
-//   contact: CommunicationMethods[K] // 💡turning key into value -- a *mapped type*
-// ) {
-//   //...
-// }
-// contact("email", { name: "foo", email: "mike@example.com" });
-// contact("phone", { name: "foo", phone: 3213332222 });
-// contact("fax", { fax: 1231 });
+function contact<K extends keyof CommunicationMethods>(
+  method: K,
+  contact: CommunicationMethods[K] // 💡turning key into value -- a *mapped type*
+) {
+  //...
+}
+contact("email", { name: "foo", email: "mike@example.com" });
+contact("phone", { name: "foo", phone: 3213332222 });
+contact("fax", { fax: 1231 });
 
 // // we can get all values by mapping through all keys
-// type AllCommKeys = keyof CommunicationMethods;
-// type AllCommValues = CommunicationMethods[keyof CommunicationMethods];
+type AllCommKeys = keyof CommunicationMethods;
+type AllCommValues = CommunicationMethods[keyof CommunicationMethods];
 
 /**
  * (2) Type queries allow us to obtain the type from a value using typeof
  */
 
-// const alreadyResolvedNum = Promise.resolve(4);
+const alreadyResolvedNum = Promise.resolve(4);
 
-// type ResolveType = typeof Promise.resolve;
+type ResolveType = typeof Promise.resolve;
 
 // const x: ResolveType = Promise.resolve;
 // x(42).then(y => y.toPrecision(2));
